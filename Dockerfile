7a1b8c2ceb79821eee0358aa05266a6035bf1cf6
Alexey Balakhanov <alexeybalahanov@gmail.com>
2022-11-23 10:11:43 +0300

    add postgres by docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM python:3.10.8
+FROM python:3.10.8-alpine
 
 WORKDIR /app
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+
+#RUN apk update \
+#    && apk add postgresql-dev gcc python3-dev musl-dev
 
 RUN pip install --upgrade pip
 COPY ./requirements.txt .
--- innotter/settings.py
+++ innotter/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-l*!3v1d-v*%w(dohh!n3v%vt5#i7a$@w40%2vn&on+kf-63a+o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
+# DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
+# ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'innotter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_01',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
