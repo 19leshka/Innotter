ce207ff457fb7f021ee219bfa19671dc99aef4eb
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-11-28 17:32:58 +0300

    add user crud

--- innotter/settings.py
+++ innotter/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user.apps.UserConfig',
     'page.apps.PageConfig',
     'post.apps.PostConfig',
@@ -117,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
