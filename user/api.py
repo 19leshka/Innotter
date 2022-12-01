ed41ecce4ad8009d190db972c3c7cc9cd1d3b7a5
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-01 15:06:20 +0300

    add user profile update

--- user/api.py
+++ user/api.py
@@ -1,34 +1,38 @@
-from rest_framework import viewsets, permissions, status
-from rest_framework.generics import RetrieveUpdateAPIView
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import (
-    LoginSerializer, RegistrationSerializer, UserSerializer,
-)
-from .renderers import UserJSONRenderer
 from django.http import HttpRequest, HttpResponse
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.viewsets import ModelViewSet
 
+from .renderers import UserJSONRenderer
 from .serializers import RegistrationSerializer
-from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework import exceptions
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from user.serializers import UserSerializer
 from user.utils import generate_access_token, generate_refresh_token
 from .models import User
 
 
 class UserView(ModelViewSet):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (UserJSONRenderer,)
 
     @action(detail=False)
     def profile(self, request: HttpRequest) -> HttpResponse:
         user = request.user
         serialized_user = UserSerializer(user).data
         return Response({'user': serialized_user})
+
+    @action(detail=False, methods=['patch'], serializer_class=UserSerializer)
+    def update_profile(self, request: HttpRequest) -> HttpResponse:
+        serializer_data = request.data.get('user', {})
+        serializer = self.serializer_class(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class AuthAPIView(ModelViewSet):
@@ -71,24 +75,3 @@ class AuthAPIView(ModelViewSet):
         }
 
         return response
-
-
-class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    renderer_classes = (UserJSONRenderer,)
-    serializer_class = UserSerializer
-
-    def retrieve(self, request, *args, **kwargs):
-        serializer = self.serializer_class(request.user)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, *args, **kwargs):
-        serializer_data = request.data.get('user', {})
-        serializer = self.serializer_class(
-            request.user, data=serializer_data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
--- user/serializers.py
+++ user/serializers.py
@@ -8,6 +8,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'is_active']
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
