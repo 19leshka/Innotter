5f4dd63fdf773cab384f5684e5467874af71b38f
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-01 17:13:52 +0300

    add tag app

--- page/models.py
+++ page/models.py
@@ -1,27 +1,34 @@
 from django.db import models
+from tag.models import Tag
+
 
 class Page(models.Model):
-   name = models.CharField(max_length=80)
-   uuid = models.CharField(max_length=30, unique=True)
-   description = models.TextField()
-   tags = models.ManyToManyField('Tag', related_name='pages')
+    name = models.CharField(max_length=80)
+    uuid = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='pages')
 
-   owner = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='pages')
-   followers = models.ManyToManyField('user.User', related_name='follows')
+    owner = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='pages')
+    followers = models.ManyToManyField('user.User', related_name='follows')
 
-   image = models.URLField(null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
 
-   is_private = models.BooleanField(default=False)
-   follow_requests = models.ManyToManyField('user.User', related_name='requests')
+    is_private = models.BooleanField(default=False)
+    follow_requests = models.ManyToManyField('user.User', related_name='requests')
 
-   unblock_date = models.DateTimeField(null=True, blank=True)
+    unblock_date = models.DateTimeField(null=True, blank=True)
 
-   def __str__(self):
-       return self.title
+    def __str__(self):
+        return self.title
 
+    def create_page(self, name: str, description: str):
+        if name is None:
+            raise TypeError('Pages must have a name.')
 
-class Tag(models.Model):
-   name = models.CharField(max_length=30, unique=True)
+        if description is None:
+            raise TypeError('Pages must have a description.')
 
-   def __str__(self):
-       return self.title
+        page = self.model(name=name, description=description)
+        page.save()
+
+        return page
