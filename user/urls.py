67b94b425f2ae9d16fabf74ce7264bb15d8a967f
Alexey Balakhanov <81266240+19leshka@users.noreply.github.com>
2022-12-01 15:19:36 +0300

    Revert "Feature/user crud"

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import RegistrationAPIView
-
-# router = routers.DefaultRouter()
-# router.register('user/', RegistrationAPIView.as_view(), 'user-registration')
-# # router.register('api/user', UserViewSet, 'user')
-#
-# urlpatterns = router.urls
