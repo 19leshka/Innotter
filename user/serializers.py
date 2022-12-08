87743bb3811fc27eaae7fbbb2e4854404cd34138
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-08 17:48:12 +0300

    add image checker in serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
+from innotter.services import AwsService
 from user.models import User
+
+
+ALLOWED_IMAGE_EXTENSIONS = ('png', 'jpg', 'jpeg')
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'is_active']
+        fields = ['id', 'username', 'email', 'password', 'is_active', 'image']
 
     def update(self, instance, validated_data):
         password = validated_data.get('password')
@@ -37,7 +41,17 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', 'image']
 
     def create(self, validated_data):
+        if 'image' in validated_data:
+            if validated_data.image.rsplit('.')[-1].lower() not in ALLOWED_IMAGE_EXTENSIONS:
+                raise serializers.ValidationError(
+                    {'status': f'Invalid uploaded image type: {validated_data.image}'}
+                )
+
+            image = AwsService.upload_file(validated_data.image, 'user' + str(User.objects.latest('id').id + 1))
+
+            validated_data.image = image
+
         return User.objects.create_user(**validated_data)
