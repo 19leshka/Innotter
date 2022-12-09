93d4fa8d094604027cdb995eb47963b97d0e9dea
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-09 15:32:38 +0300

    create services

--- innotter/services.py
+++ innotter/services.py
@@ -1,28 +1,32 @@
 import logging
 import os
 import boto3
-from botocore.exceptions import ClientError
 
 
 class AwsService:
     @staticmethod
-    def upload_file(file_path: str, key: str) -> str:
+    def get_credentials():
         credentials = {
             'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
             'aws_secret_access_key': os.getenv('AWS_SECRET_KEY'),
         }
-        client_s3 = boto3.client('s3', **credentials)
+        return credentials
+
+    @staticmethod
+    def get_client():
+        credentials = AwsService.get_credentials()
+        return boto3.client('s3', **credentials)
+
+    @staticmethod
+    def upload_file(file_path: str, key: str) -> str:
+        client_s3 = AwsService.get_client()
         client_s3.upload_fileobj(file_path, Bucket=os.getenv('BUCKET_NAME'), Key=key)
 
         return key
 
     @staticmethod
     def get_file_url(key: str = None):
-        credentials = {
-            'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
-            'aws_secret_access_key': os.getenv('AWS_SECRET_KEY'),
-        }
-        client_s3 = boto3.client('s3', **credentials)
+        client_s3 = AwsService.get_client()
 
         try:
             response = client_s3.generate_presigned_url('get_object',
