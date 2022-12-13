d85c8dcc4e49b9b7fb2fc6209260f98fd76ec48f
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-13 15:54:36 +0300

    change celery app

--- innotter/celery.py
+++ innotter/celery.py
@@ -1,11 +1,8 @@
 import os
 from celery import Celery
-from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innotter.settings')
-
 app = Celery('innotter')
-
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
