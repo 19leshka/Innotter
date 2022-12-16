1baf82de177b7f990e52804eb2e8adc51213be08
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-16 14:25:20 +0300

    rewrite consumer

--- microservice/consumer.py
+++ microservice/consumer.py
@@ -1,50 +1,40 @@
-import json
-import os
-from enum import Enum
+import json, os, logger, pika
 
-import pika
-
-from microservice.services import DynamoDBService
+from aio_pika import connect_robust
 
 
-class MessageType(Enum):
-    CREATE = 'create'
-    UPDATE = 'update'
-    DELETE = 'delete'
+class PikaClient:
+    def __init__(self, process_callable):
+        params = pika.URLParameters(os.getenv('AMQP_URL'))
+        self.publish_queue_name = os.getenv('PUBLISH_QUEUE')
+        self.connection = pika.BlockingConnection(params)
+        self.channel = self.connection.channel()
+        self.publish_queue = self.channel.queue_declare(queue=self.publish_queue_name)
+        self.callback_queue = self.publish_queue.method.queue
+        self.response = None
+        self.process_callable = process_callable
 
-    def __str__(self):
-        return self.value
+    async def consume(self, loop):
+        connection = await connect_robust(os.getenv('AMQP_URL'), loop=loop)
+        channel = await connection.channel()
+        queue = await channel.declare_queue(os.getenv('CONSUME_QUEUE'))
+        await queue.consume(self.process_incoming_message, no_ack=False)
+        return connection
 
+    async def process_incoming_message(self, message):
+        async with message.process():
+            try:
+                message = json.loads(message.body.decode())
+                # message_type = message['type']
+                #
+                # if message_type == MessageType.CREATE.value:
+                #     AWSManager.put_item(message)
+                #
+                # elif message_type == MessageType.UPDATE.value:
+                #     AWSManager.update_item(message)
+                #
+                # elif message_type == MessageType.DELETE.value:
+                #     AWSManager.delete_item(message)
 
-def consume():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=os.getenv('RABBITMQ_HOST')
-    ))
-    channel = connection.channel()  # start a channel
-    channel.queue_declare(queue=os.getenv('PUBLISH_QUEUE'))
-
-    channel.basic_consume(os.getenv('PUBLISH_QUEUE'),
-                          callback,
-                          auto_ack=True)
-
-    channel.start_consuming()
-    connection.close()
-
-
-def callback(message):
-    with message.process():
-        try:
-            message = json.loads(message.body.decode())
-            message_type = message['type']
-
-            if message_type == MessageType.CREATE.value:
-                DynamoDBService.put_item(message)
-
-            elif message_type == MessageType.UPDATE.value:
-                DynamoDBService.update_item(message)
-
-            elif message_type == MessageType.DELETE.value:
-                DynamoDBService.delete_item(message)
-
-        except Exception as e:
-            raise Exception(e)
+            except Exception as e:
+                logger.error(e)
