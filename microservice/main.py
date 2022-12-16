058e68f1065670519ffcb2680effb1b04aa8b14b
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-16 14:26:28 +0300

    change main fastapi

--- microservice/main.py
+++ microservice/main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+import asyncio
 
+from fastapi import FastAPI
 from router import router
 
-app = FastAPI()
+from consumer import PikaClient
+
+
+#
+#
+class FooApp(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pika_client = PikaClient(self.log_incoming_message)
+
+
+app = FooApp()
 app.include_router(router)
 
 
-@app.get("/hi")
-def read_root():
-    return {"Hello": "world"}
+@app.on_event('startup')
+async def startup():
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(app.pika_client.consume(loop))
+    await task
