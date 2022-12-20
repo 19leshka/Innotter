4e94f23d6f14f9d71c6179a742b8155485f41b91
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-20 14:33:15 +0300

    add redux and redux thunk

---
+++ innotter-front/src/redux/store.ts
@@ -0,0 +1,12 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+
+const rootReducer = combineReducers({
+
+})
+
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+export default store
