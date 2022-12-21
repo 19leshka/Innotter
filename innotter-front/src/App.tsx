84cfae2212e427c8fc4d3c145f2172f9a826962a
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-21 22:51:57 +0300

    refactor

--- innotter-front/src/App.tsx
+++ innotter-front/src/App.tsx
@@ -1,29 +1,40 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route ,useNavigate} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import { useTypedSelector } from "./hooks/useTypedSelector"
 import store from './redux/store'
 import Login from './components/Login'
 import Register from './components/Register'
+import Header from './components/Header'
 
 const App:React.FC = () => {
-  return (
-    <div>
-      <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-      </Routes>
-    </div>
-  );
+    const navigate = useNavigate();
+    const isAuth = useTypedSelector(state => state.auth.isAuth);
+    React.useEffect(() => {
+        if(!isAuth) navigate(`/login`);
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <Routes>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+            </div> 
+        </div>
+    );
 }
 
 const Innotter:React.FC = () => {
-  return (
-    <BrowserRouter> 
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter> 
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    )
 }
 
 export default Innotter;
