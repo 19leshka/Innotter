3ddf27d3b500e50655a59a82610a56768a4af4fc
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-23 01:24:25 +0300

    create mock tests for aws s3 and ses

--- innotter/tests/test_services.py
+++ innotter/tests/test_services.py
@@ -6,23 +6,23 @@ from innotter.services import AwsService
 
 class TestAwsServiceMock(unittest.TestCase):
 
-    @patch('microservice.services.AwsService.upload_file')
-    def test_put_item(self, mock_upload_file):
+    @patch('innotter.services.AwsService.upload_file')
+    def test_upload_file(self, mock_upload_file):
         mock_upload_file.return_value = 'user1'
 
-        result = AwsService.put_item('filepath')
+        result = AwsService.upload_file('filepath')
 
         self.assertEqual(result, 'user1')
 
-    @patch('microservice.services.AwsService.get_file_url')
-    def test_put_item(self, mock_get_file_url):
+    @patch('innotter.services.AwsService.get_file_url')
+    def test_get_file(self, mock_get_file_url):
         mock_get_file_url.return_value = 'link'
 
-        result = AwsService.put_item('user1')
+        result = AwsService.get_file_url('user1')
 
         self.assertEqual(result, 'link')
 
-    @patch('microservice.services.AwsService.send_email')
+    @patch('innotter.services.AwsService.send_email')
     def send_email(self, mock_send_email):
         mock_send_email.return_value = 'done'
 
