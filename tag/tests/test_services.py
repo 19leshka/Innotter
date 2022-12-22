fe66dd76b3d76ec7f9cb49e31f9a6a67fc63d96d
Alexey Balahanov <alexeybalahanov@gmail.com>
2022-12-22 22:45:26 +0300

    create working tests for tag service

--- tag/tests/test_services.py
+++ tag/tests/test_services.py
@@ -1,28 +1,25 @@
-from unittest import TestCase
-from unittest.mock import patch
-
+from django.test import RequestFactory, TestCase
 from tag.services import TagService
 
 
-class TestPageServiceMock(TestCase):
-    @patch('tag.services.TagService.process_tags')
-    def test_put_item(self, mock_process_tags):
+class TestPageService(TestCase):
+    def test_put_item(self):
         test_cases = (
             {
-                'expected': [1,2,3],
-                'arguments': ['sport', 'games', 'news']
+                'expected': [1, 2, 3],
+                'arguments': ['sport', 'games', 'news'],
             },
             {
-                'expected': [2,3],
-                'arguments': ['games', 'news']
+                'expected': [2, 3],
+                'arguments': ['games', 'news'],
             },
             {
-                'expected': [1,4],
-                'arguments': ['sport', 'movies']
+                'expected': [1, 4],
+                'arguments': ['sport', 'movies'],
             },
         )
 
         for test_case in test_cases:
-            mock_process_tags.return_value = test_case['expected']
-            result = TagService.process_tags({'data': {'tags': test_case['arguments']}})
+            request = RequestFactory().post(path='/pages', data={'tags': test_case['arguments']})
+            result = TagService.process_tags(request)
             self.assertEqual(result, test_case['expected'])
